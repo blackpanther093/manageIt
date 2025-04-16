00034745ea90a3cbeb0653ce60e7a2f3970c1a67
JatIn <147529902+blackpanther093@users.noreply.github.com>
2025-04-16 20:15:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,6 @@ from routes import app
 dotenv_path = os.path.join(os.path.dirname(__file__), 'myenv', '.env')
 load_dotenv(dotenv_path=dotenv_path)
 
-DB_PORT = os.getenv('DB_PORT')
-
 # app = Flask(__name__)
 # app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
 # jwt = JWTManager(app)
@@ -23,4 +21,5 @@ app.secret_key = os.getenv('SECRET_KEY', os.urandom(24))
 # jwt = JWTManager(app)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=DB_PORT)
+    port = os.getenv('DB_PORT')
+    app.run(host="0.0.0.0", port=port)
